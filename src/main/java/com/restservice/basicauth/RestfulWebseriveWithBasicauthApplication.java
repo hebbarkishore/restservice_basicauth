4c1503f9f99e58aa4d48d9b34eea1e0c28520e6d
hebbaki <khebbar@jhancock.com>
2020-07-01 17:46:33 -0400

    Adding the Application file

---
+++ src/main/java/com/restservice/basicauth/RestfulWebseriveWithBasicauthApplication.java
@@ -0,0 +1,13 @@
+package com.restservice.basicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebseriveWithBasicauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebseriveWithBasicauthApplication.class, args);
+	} 
+
+}
